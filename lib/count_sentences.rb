8a5a27495e5df6526c107982533f5a3275a69442
Matt Eva <matteweva@gmail.com>
2021-11-23 16:42:53 -0800

    commit tree 5ea42f8ac7740a4690112db43d305435509d9acf

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,23 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_self = self.split(/[!?\.]+/)
+    split_self.length
+    # binding.pry
   end
 end
+
+"This, well, is a sentence. This is too!! And so is this, I think? Woo...".count_sentences
